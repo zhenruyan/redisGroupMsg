7f159cfeaf631838f0a5a4ca2632f95917b38e45
白洋 <1263580126@qq.com>
2018-11-29 18:37:20 +0800

    整理

--- redisGroupMsg/lib.py
+++ redisGroupMsg/lib.py
@@ -1,20 +1,29 @@
 import redis
 
-pool = redis.ConnectionPool(host='127.0.0.1', port=6379)
-r = redis.Redis(connection_pool=pool,db=0)
 
 class redisMessage():
-    def __init__(self):
-        self.r = r
-        file = open("./groupsend.lua")
-        script = file.read()
-        self.hash = self.r.script_load(script)
+    def __init__(self,host='127.0.0.1', port=6379,db=0):
+        self.pool = redis.ConnectionPool(host,port)
+        self.conn = redis.Redis(connection_pool=pool,db)
+        self.script ="""
+        local status = true
+        local groupname = KEYS[1]
+        local msg = ARGV[1]
+        local grouplist = redis.call("SMEMBERS",groupname)
+        for item  in ipairs(grouplist) do
+        status = redis.call("lpush",item,msg)
+        end
+        return status
+        """
+        self.hash = self.conn.script_load(self.script)
 
     def addGroup(self,group,name):
-        self.r.sadd(group,name)
+        self.conn.sadd(group,name)
 
+    def removeGroup(self,group,name):
+        self.conn.srem(group,name)
 
     def sendGroup(self,group,msg):
         args = (group, msg)
-        resout = r.evalsha(self.hash, 1, *args)
+        resout = self.conn.evalsha(self.hash, 1, *args)
         return  resout
