a452c121e37c8ae96c232d5b1b114642c6270608
zheruyan <admin@pkold.com>
2018-11-29 22:23:33 +0800

    version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='redisGroupMsg',
-      version='0.0.4',
+      version='0.0.5',
       description='This is a packet that broadcasts redis multiple queues',
       url='https://github.com/zhenruyan/redisGroupMsg',
       author='zhenruyan',
